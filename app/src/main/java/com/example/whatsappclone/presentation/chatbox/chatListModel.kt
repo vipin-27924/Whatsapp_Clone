63992ebe0284f666d71da933e0cdffd06622e52d
Vipin kumar <vipinkumar27924@gmail.com>
2025-05-20 11:06:23 +0530

    Chat Model

--- app/src/main/java/com/example/whatsappclone/presentation/chatbox/chatListModel.kt
+++ app/src/main/java/com/example/whatsappclone/presentation/chatbox/chatListModel.kt
@@ -1,3 +1,5 @@
 package com.example.whatsappclone.presentation.chatbox
 
-data class chatListModel(val Image: Int, val name: String?, val Time: String?, val message: String?)
+data class chatListModel(
+    val Image: Int, val name: String?, val Time: String?, val message: String?
+)
--- app/src/main/java/com/example/whatsappclone/presentation/updatescreen/topBar.kt
+++ app/src/main/java/com/example/whatsappclone/presentation/updatescreen/topBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -16,6 +17,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
@@ -46,8 +49,14 @@ fun TopBar(){
                             focusedContainerColor = Color.Transparent,
                             unfocusedIndicatorColor = Color.Transparent,
                             focusedIndicatorColor = Color.Transparent,
-                            cursorColor = Color.Black
-                        ))
+                            cursorColor = Color.Black,
+                        ),
+                        modifier = Modifier
+                            .padding(start = 12.dp),
+                        singleLine = true
+                    )}else{
+                        Text(text  = "Updates",
+                            fontSize = 12.sp)
                 }
             }
         }
